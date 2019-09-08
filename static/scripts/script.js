d394919e8f58cac9a76af2b4b20645541257ce86
plin0009 <plin0009@gmail.com>
2019-09-08 07:19:35 -0400

    Modified the stock display

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -15,6 +15,17 @@ window.onload = () => {
             imgSrc.push(stock + '.png');
             imgSrc = imgSrc.join('/');
             document.getElementById('stockImage').src = imgSrc;
+            document.getElementById('stockInfo').innerHTML = stocksInfo[stock] || "Sorry, no stock information available";
         });
     }
+}
+
+stocksInfo = {
+    UPS: 'Stock prices will most likely increase slightly. It is advisable to invest in this stock.',
+    AAPL: 'Stock prices will most likely decrease slightly. It is not advisable to invest in this stock.',
+    AMZN: 'Stock prices will most likely remain stable. It is advisable to find a better stock.',
+    COST: 'Stock prices will most likely decrease heavily. It is not advisable to invest in this stock.',
+    CVS: 'Stock prices will most likely fluctuate but remain stable. It is advisable to find a better stock.',
+    EBAY: 'Stock prices will most likely remain stable. It is advisable to find a better stock.',
+    IBM: 'Stock prices will most likely remain stable. It is advisable to find a better stock.',
 }
