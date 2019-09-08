b7bc2aebcba1bc3f855fd3acd58f76b6d189f193
plin0009 <plin0009@gmail.com>
2019-09-08 07:06:45 -0400

    Stocks on dashboard

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -3,4 +3,18 @@ window.onload = () => {
         document.getElementById("navBurger").classList.toggle('is-active');
         document.getElementById("navMenu").classList.toggle('is-active');
     });
+
+    let stockSelectors = document.getElementsByClassName('stock-selector');
+    for (let i = 0; i < stockSelectors.length; i++) {
+        stockSelectors.item(i).addEventListener('click', e => {
+            const stock = e.target.innerHTML;
+            document.getElementById('stockName').innerHTML = stock;
+            let imgSrc = document.getElementById('stockImage').src;
+            imgSrc = imgSrc.split('/');
+            imgSrc.pop();
+            imgSrc.push(stock + '.png');
+            imgSrc = imgSrc.join('/');
+            document.getElementById('stockImage').src = imgSrc;
+        });
+    }
 }
